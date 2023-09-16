1e1360e492652e2359a0b0710a8cf73058b3b716
Caio Macedo <caiomacedo0102@gmail.com>
2023-09-16 20:55:15 -0300

    Refactored HibernateExceptionHandler.java to be more readable

--- src/main/java/com/macedocaio/customermanager/exceptions/handler/HibernateExceptionHandler.java
+++ src/main/java/com/macedocaio/customermanager/exceptions/handler/HibernateExceptionHandler.java
@@ -18,22 +18,45 @@ public class HibernateExceptionHandler {
 
     @ExceptionHandler(value = MethodArgumentNotValidException.class)
     @ResponseStatus(value = HttpStatus.BAD_REQUEST)
-    public Map<String, List<ErrorMessage>> handleValidationException(MethodArgumentNotValidException ex) {
-        Map<String, List<ErrorMessage>> errors = new HashMap<>();
-        ex.getBindingResult().getAllErrors().forEach((error) -> {
-            String fieldName = ((FieldError) error).getField();
-            ErrorMessage errorMessage = new ErrorMessage(error.getDefaultMessage());
+    public Map<String, List<ErrorMessage>> handleMethodArgumentNotValidException(MethodArgumentNotValidException ex) {
+        return mapFieldErros(ex);
+    }
 
-            List<ErrorMessage> errosList = new ArrayList<>();
-            if (errors.containsKey(fieldName)) {
-                errosList.addAll(errors.get(fieldName));
-                errosList.add(errorMessage);
-            } else {
-                errosList.add(errorMessage);
-                errors.put(fieldName, errosList);
-            }
-            errors.put(fieldName, errosList);
-        });
-        return errors;
+    /**
+     * Realiza o mapeamento dos erros lançados pelas anotações do jakarta.validation
+     * @param ex Exceção lançada quando alguma validação encontra erro
+     * @return {@link Map} contendo as mensagens de erro por nome do campo
+     */
+    private Map<String, List<ErrorMessage>> mapFieldErros(MethodArgumentNotValidException ex) {
+        Map<String, List<ErrorMessage>> messagesMap = new HashMap<>();
+        List<FieldError> fieldErrors = ex.getBindingResult().getFieldErrors();
+
+        for (FieldError fieldError : fieldErrors) {
+            String field = fieldError.getField();
+            String message = fieldError.getDefaultMessage();
+
+            ErrorMessage errorMessage = new ErrorMessage(message);
+            createOrAddError(field, errorMessage, messagesMap);
+        }
+        return messagesMap;
+    }
+
+    /**
+     * Cria ou adiciona um erro ao mapa de mensagens de erro
+     * @param field Nome do campo com erro
+     * @param errorMessage Objeto contendo mensagem e horario do erro
+     * @param messagesMap {@link Map} contendo as mensagens de erro por nome do campo
+     */
+    private void createOrAddError(String field, ErrorMessage errorMessage,
+                                  Map<String, List<ErrorMessage>> messagesMap) {
+
+        List<ErrorMessage> errors = new ArrayList<>();
+        if (messagesMap.containsKey(field)) {
+            errors.addAll(messagesMap.get(field));
+            errors.add(errorMessage);
+        } else {
+            errors.add(errorMessage);
+        }
+        messagesMap.put(field, errors);
     }
 }
